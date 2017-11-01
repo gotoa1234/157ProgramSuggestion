975b23b21a3e5854e43d7c528073e4f075b9dafd
gotoa1234 <cap8825@gmail.com>
2017-11-02 01:22:31 +0800

    004TryParseAndParse
    004.TryParse比Parse好

---
+++ 004TryParseAndParse/004TryParseAndParse/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace _004TryParseAndParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Perfomance = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_Perfomance
+            // 
+            this.textBox_Perfomance.Location = new System.Drawing.Point(12, 12);
+            this.textBox_Perfomance.Multiline = true;
+            this.textBox_Perfomance.Name = "textBox_Perfomance";
+            this.textBox_Perfomance.Size = new System.Drawing.Size(260, 237);
+            this.textBox_Perfomance.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox_Perfomance);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Perfomance;
+    }
+}
+
---
+++ 004TryParseAndParse/004TryParseAndParse/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _004TryParseAndParse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Parse 效能
+            textBox_Perfomance.AppendText(this.parseSuccessful());
+            textBox_Perfomance.AppendText(this.parseFailure());
+
+            //TryParse 效能
+            textBox_Perfomance.AppendText(this.tryParseSuccessful());
+            textBox_Perfomance.AppendText(this.tryParseFailure());
+
+        }
+
+        /// <summary>
+        /// .Parse() 跑1000次，且成功
+        /// </summary>
+        /// <returns></returns>
+        private string parseSuccessful()
+        {
+            //計時開始
+            Stopwatch sw = Stopwatch.StartNew();
+            double tempValue = 0;
+            for (int i = 1000; i > 0; i--)
+            {
+                try
+                {
+                    tempValue = double.Parse("123");
+                }
+                catch (Exception ex)
+                {
+                    tempValue = 0;
+                }
+            }
+            //計時結束
+            sw.Stop();
+            return string.Format("parseSuccessful: {0}{1}", sw.Elapsed.ToString(), "\r\n");
+        }
+
+        /// <summary>
+        /// .Parse() 跑1000次，且失敗
+        /// </summary>
+        /// <returns></returns>
+        private string parseFailure()
+        {
+            //計時開始
+            Stopwatch sw = Stopwatch.StartNew();
+            double tempValue = 0;
+            for (int i = 1000; i > 0; i--)
+            {
+                try
+                {
+                    tempValue = double.Parse(null);
+                }
+                catch (Exception ex)
+                {
+                    tempValue = 0;
+                }
+            }
+            //計時結束
+            sw.Stop();
+            return string.Format("parseFailure : {0}{1}", sw.Elapsed.ToString(), "\r\n");
+        }
+
+
+        /// <summary>
+        /// .TryParse() 跑1000次，且成功
+        /// </summary>
+        /// <returns></returns>
+        private string tryParseSuccessful()
+        {
+            //計時開始
+            Stopwatch sw = Stopwatch.StartNew();
+            double tempValue = 0;
+            for (int i = 1000; i > 0; i--)
+            {
+                if (double.TryParse("123", out tempValue) == false)
+                {
+                    tempValue = 0;
+                }
+            }
+            //計時結束
+            sw.Stop();
+            return string.Format("tryParseSuccessful: {0}{1}", sw.Elapsed.ToString(), "\r\n");
+        }
+
+        /// <summary>
+        /// .TryParse() 跑1000次，且失敗
+        /// </summary>
+        /// <returns></returns>
+        private string tryParseFailure()
+        {
+            //計時開始
+            Stopwatch sw = Stopwatch.StartNew();
+            double tempValue = 0;
+            for (int i = 1000; i > 0; i--)
+            {
+                if (double.TryParse(null, out tempValue) == false)
+                {
+                    tempValue = 0;
+                }
+            }
+            //計時結束
+            sw.Stop();
+            return string.Format("tryParseFailure: {0}{1}", sw.Elapsed.ToString(), "\r\n");
+        }
+    }
+}
