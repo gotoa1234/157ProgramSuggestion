67a91f14151865d3e099cac9b49ba306a9871eb1
gotoa1234 <cap8825@gmail.com>
2017-11-06 23:04:08 +0800

    005intSetNull
    005.使用 int? 來確保數值型別也可以為null (依照情境決定變數是否可為<nulllable>)

---
+++ 005intSetNull/005intSetNull/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _005intSetNull
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //可為Null的型別變數 可以直接對型別變數存放值 (隱式存值)
+            //Nullable<int> score = null;//完整定義
+            int? score = null;//語法糖，簡略定義
+            int j = 0;
+            score = j;//Nullable<int>  = int :可以這樣寫
+
+            //但可為Null的型別變數 不可被型別變數存放值
+
+            int? score2 = 123;
+            int j2 = 0;
+            //j2 = score2; 該行出錯   ==> int = Nullable<int>  :不可以這樣寫
+
+            //正確做法
+            j2 = score2 ?? 0;
+
+            //如同
+            j2 = score2.HasValue == true ? 0 :j2;
+
+            //如同
+            if (score2.HasValue == true)
+            {
+                j2 = score2.Value;
+            }
+            
+             
+        }
+
+    }
+}
