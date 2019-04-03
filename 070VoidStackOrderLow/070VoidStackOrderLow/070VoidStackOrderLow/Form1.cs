b67129c8c65042472ab8ffa89a1a2c3278c640d1
gotoa1234 <cap8825@gmail.com>
2019-04-03 21:26:03 +0800

    070. 引用避免再堆疊棧順序較低的位置紀錄異常 (在代碼中包覆的例外避免巢狀重複)

---
+++ 070VoidStackOrderLow/070VoidStackOrderLow/070VoidStackOrderLow/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _070VoidStackOrderLow
+{
+    /// <summary>
+    /// 070. 引用避免再堆疊棧順序較低的位置紀錄異常 (在代碼中包覆的例外避免巢狀重複)
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MethodHigh();
+        }
+
+        internal void MethodLow()
+        {
+            try
+            {
+                int a = 0;
+                int b = 0;
+                int c = a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($@"MethodLow: {ex.Message}");
+                throw;
+            }
+        }
+
+        internal void MethodHigh()
+        {
+            try
+            {
+                MethodLow();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($@"MethodHigh: {ex.Message}");
+            }
+        }
+    }
+}
