52668005e20f0e9b3d58772eede11b94ff9097e7
gotoa1234 <cap8825@gmail.com>
2019-04-08 22:25:13 +0800

    074. 警惕線程的IsBackground (true:後台Thread 應用程式結束跟著關閉，false:前台Thread 應用程式結束仍存在，除非本身Thread執行完畢)

---
+++ 074WatchOutIsBackground/074WatchOutIsBackground/074WatchOutIsBackground/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace _074WatchOutIsBackground
+{
+    /// <summary>
+    /// 074. 警惕線程的IsBackground (true:後台Thread 應用程式結束跟著關閉，false:前台Thread 應用程式結束仍存在，除非本身Thread執行完畢)
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() =>
+            {
+                Console.WriteLine("Thread Working");
+                Console.ReadKey();
+                Console.WriteLine("Thread Finish");
+
+            });
+            Console.WriteLine("主程式結束");
+
+            t.IsBackground = false;
+            t.Start();
+        }
+    }
+}
