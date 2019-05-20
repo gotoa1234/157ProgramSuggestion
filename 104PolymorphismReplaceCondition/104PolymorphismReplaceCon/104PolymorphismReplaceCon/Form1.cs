51b2dd9282c9460201d1fb2188874b037bc21db1
gotoa1234 <cap8825@gmail.com>
2019-05-20 13:49:44 +0800

    104 : 用多態代替條件語句 105: 使用私有構造函數強化單例 106: 為靜態類添加靜態構造函數

---
+++ 104PolymorphismReplaceCondition/104PolymorphismReplaceCon/104PolymorphismReplaceCon/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _104PolymorphismReplaceCon
+{
+    /// <summary>
+    /// 104 : 用多態代替條件語句
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void NormalCondition()
+        {
+            
+        }
+
+        private void PolymorphismReplaceExample()
+        {
+            //啟動
+            Commander commander = new StartCommander();
+            Driver(commander);
+
+            //停止
+            commander = new StopCommander();
+            Driver(commander);
+        }
+
+        private void Driver(Commander com)
+        {
+            com.Execute();
+        }
+
+
+        abstract class Commander
+        {
+            public abstract void Execute();
+        }
+
+        class StartCommander : Commander
+        {
+            public override void Execute()
+            {
+                //啟動
+            }
+        }
+
+        class StopCommander : Commander
+        {
+            public override void Execute()
+            {
+                //停止
+            }
+ 
+        }
+    }
+}
---
+++ 106StaticAddStaticStruceMethod/106StaticAddStaticStruceMethod/106StaticAddStaticStruceMethod/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _106StaticAddStaticStruceMethod
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 106: 為靜態類添加靜態構造函數
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
